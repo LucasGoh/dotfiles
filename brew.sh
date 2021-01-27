9458514eb6ca1c76a9cc162746d659afeb8ed083
LucasGoh <leftcompact@gmail.com>
2021-01-28 08:36:00 +0900

    n

--- brew.sh
+++ brew.sh
@@ -11,18 +11,18 @@ brew install htop
 
 #plugin: ctrlpvim.github.io/ctrlp.vim
 brew install vim
-
 brew install tmux
 echo "set -g mouse on" >> ~/tmux.conf //-g copy default conf and add something, enable use mose to scroll.
+
+#install app 
 brew install youtube-dl
 brew install gnupg
 brew install pstree
-brew install task
-brew install timewarrior
 
 
 brew install --cask spotify
 brew install --cask chromium
+brew install --cask vlc
 
 : '
  #install on appStore:  
@@ -33,4 +33,8 @@ brew install --cask chromium
  wpsoffice
  anki
  telegram
+'
+:'
+ #install on web
+ tiles
 '
